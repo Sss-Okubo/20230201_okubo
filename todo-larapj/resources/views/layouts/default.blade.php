fab98d45f4e5d454b5f3122a030f1c012abb5a48
Sss-Okubo <118349010+Sss-Okubo@users.noreply.github.com>
2023-02-01 11:12:48 +0900

    Update default.blade.php

--- todo-larapj/resources/views/layouts/default.blade.php
+++ todo-larapj/resources/views/layouts/default.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>作成中</title>
+  <title>COACHTECH</title>
   <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
   <style>
     .container {
